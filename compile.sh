b35501831ea4c54864c04c907891949f57bffcd9
Ben-401 <gerb0002@flinders.edu.au>
2016-12-22 15:12:22 +1030

    added container.ut as bitgen needs it

--- compile.sh
+++ compile.sh
@@ -114,9 +114,8 @@ xst ${ISE_COMMON_OPTS} -ifn "container.xst" -ofn "${SCROUTDIR_FULL}/container.sy
 #xst ${ISE_COMMON_OPTS} -ifn "${BASEDIR}/isepn147/mega65/working/container.xst" -ofn "${BASEDIR}/build/container.syr" >> $outfile1
 retcode=$?
 if [ $retcode -ne 0 ] ; then
-  echo "cst failed with return code $retcode"
-  cat $outfile1 | grep ERROR
-  exit 1
+  echo "xst failed with return code $retcode" &&
+  cat $outfile1 | grep ERROR && exit 1
 else
   cat $outfile1 | grep WARN
 fi
@@ -126,12 +125,11 @@ fi
 #
 datetime=`date +%Y%m%d_%H:%M:%S`
 echo "==> $datetime Starting: ngdbuild, see container.bld"
-ngdbuild ${ISE_COMMON_OPTS} ${ISE_NGDBUILD_OPTS} -uc ../../../src/vhdl/container-n4ddr.ucf ${SCROUTDIR_FULL}/container.ngc ${SCROUTDIR_FULL}/container.ngd > $outfile2
+ngdbuild ${ISE_COMMON_OPTS} ${ISE_NGDBUILD_OPTS} -uc ../../../src/vhdl/container-n4ddr.ucf container.ngc ${SCROUTDIR_FULL}/container.ngd > $outfile2
 retcode=$?
 if [ $retcode -ne 0 ] ; then
-  echo "ngdbuild failed with return code $retcode"
-  cat $outfile2 | grep ERROR
-  exit 1
+  echo "ngdbuild failed with return code $retcode" &&
+  cat $outfile2 | grep ERROR && exit 1
 else
   cat $outfile2 | grep WARN
 fi
@@ -144,7 +142,10 @@ echo "==> $datetime Starting: map, see container_map.mrp"
 map ${ISE_COMMON_OPTS} ${ISE_MAP_OPTS} -o ${SCROUTDIR_FULL}/container_map.ncd ${SCROUTDIR_FULL}/container.ngd ${SCROUTDIR_FULL}/container.pcf > $outfile3
 retcode=$?
 if [ $retcode -ne 0 ] ; then
-  echo "map failed with return code $retcode" && exit 1
+  echo "map failed with return code $retcode" &&
+  cat $outfile3 | grep ERROR && exit 1
+else
+  cat $outfile3 | grep WARN
 fi
 
 #
@@ -155,7 +156,10 @@ echo "==> $datetime Starting: par, see container.par"
 par ${ISE_COMMON_OPTS} ${ISE_PAR_OPTS} ${SCROUTDIR_FULL}/container_map.ncd ${SCROUTDIR_FULL}/container.ncd ${SCROUTDIR_FULL}/container.pcf > $outfile4
 retcode=$?
 if [ $retcode -ne 0 ] ; then
-  echo "par failed with return code $retcode" && exit 1
+  echo "par failed with return code $retcode" &&
+  cat $outfile4 | grep ERROR && exit 1
+else
+  cat $outfile4 | grep WARN
 fi
 
 #
@@ -166,7 +170,10 @@ echo "==> $datetime Starting: trce, see container.twr"
 trce ${ISE_COMMON_OPTS} ${ISE_TRCE_OPTS} ${SCROUTDIR_FULL}/container.twx ${SCROUTDIR_FULL}/container.ncd -o ${SCROUTDIR_FULL}/container.twr ${SCROUTDIR_FULL}/container.pcf -ucf ../../../src/container-n4ddr.ucf > $outfile5
 retcode=$?
 if [ $retcode -ne 0 ] ; then
-  echo "trce failed with return code $retcode" && exit 1
+  echo "trce failed with return code $retcode" &&
+  cat $outfile5 | grep ERROR && exit 1
+else
+  cat $outfile5 | grep WARN
 fi
 
 #
@@ -177,7 +184,10 @@ echo "==> $datetime Starting: bitgen, see container.bgn"
 bitgen ${ISE_COMMON_OPTS} -f ${SCROUTDIR_FULL}/container.ut ${SCROUTDIR_FULL}/container.ncd > $outfile6
 retcode=$?
 if [ $retcode -ne 0 ] ; then
-  echo "bitgen failed with return code $retcode" && exit 1
+  echo "bitgen failed with return code $retcode" &&
+  cat $outfile6 | grep ERROR && exit 1
+else
+  cat $outfile6 | grep WARN
 fi
 
 #
